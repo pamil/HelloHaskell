52b1f494ff65c8138da70145828b4c7d8a92e652
Kamil Kokot <kamil@kokot.me>
2016-07-12 22:23:01 +0200

    P08: Eliminate consecutive duplicates of list elements

---
+++ src/Pamil/List/Compress.hs
@@ -0,0 +1,9 @@
+module Pamil.List.Compress where
+
+-- P08: Eliminate consecutive duplicates of list elements.
+
+compress :: (Eq a) => [a] -> [a]
+compress (x:xs@(y:_))
+  | x == y    = compress xs
+  | otherwise = x : compress xs
+compress xs = xs
---
+++ test/Pamil/List/CompressSpec.hs
@@ -0,0 +1,16 @@
+module Pamil.List.CompressSpec (main, spec) where
+
+import Test.Hspec
+import Pamil.List.Compress
+
+main :: IO ()
+main = hspec spec
+
+spec :: Spec
+spec = do
+  describe "compress" $ do
+    it "compresses string" $ do
+      compress "aaabbcdddeef" `shouldBe` "abcdef"
+
+    it "compresses an integer list" $ do
+      compress [1, 1, 2, 1, 3, 3, 3, 4, 4] `shouldBe` [1, 2, 1, 3, 4]
