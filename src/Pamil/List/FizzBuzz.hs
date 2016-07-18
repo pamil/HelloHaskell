49df18fbc75f68784f6cbd5016d9579727c86aa2
Kamil Kokot <kamil@kokot.me>
2016-07-18 21:59:28 +0200

    FizzBuzz!

---
+++ src/Pamil/List/FizzBuzz.hs
@@ -0,0 +1,10 @@
+module Pamil.List.FizzBuzz where
+
+-- Fizz Buzz!
+
+fizzBuzz :: Integer -> String
+fizzBuzz n
+  | n `mod` 15 == 0 = "FizzBuzz"
+  | n `mod` 5  == 0 = "Buzz"
+  | n `mod` 3  == 0 = "Fizz"
+  | otherwise       = show n
