efa8723cae0f9520c1abdb43c440960496d0b524
Kamil Kokot <kamil@kokot.me>
2016-07-19 01:22:32 +0200

    Coding standard fixes

--- src/Pamil/List/MyButLast.hs
+++ src/Pamil/List/MyButLast.hs
@@ -4,6 +4,6 @@ module Pamil.List.MyButLast where
 
 myButLast :: [a] -> a
 myButLast [] = error "Cannot get the last but one element of an empty list!"
-myButLast (_:[]) = error "Cannot get the last but one element of a list with only one element!"
-myButLast (x:_:[]) = x
+myButLast [_] = error "Cannot get the last but one element of a list with only one element!"
+myButLast [x, _] = x
 myButLast (_:xs) = myButLast xs
