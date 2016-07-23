a940ca8a1f4bb6f32b02e24c9cf1c042966d4986
Kamil Kokot <kamil@kokot.me>
2016-07-23 21:22:15 +0200

    Add Quicksort implementation using function composition

--- src/Pamil/List/Quicksort.hs
+++ src/Pamil/List/Quicksort.hs
@@ -7,3 +7,9 @@ quicksort [] = []
 quicksort (x:xs) = smallerOrEqual ++ [x] ++ bigger
     where smallerOrEqual = quicksort [ y | y <- xs, y <= x]
           bigger         = quicksort [ y | y <- xs, y > x]
+
+quicksort' :: (Ord a) => [a] -> [a]
+quicksort' [] = []
+quicksort' (x:xs) = smallerOrEqual ++ [x] ++ bigger
+    where smallerOrEqual = quicksort' . filter (<= x) $ xs
+          bigger         = quicksort' . filter (> x) $ xs
--- test/Pamil/List/QuicksortSpec.hs
+++ test/Pamil/List/QuicksortSpec.hs
@@ -11,3 +11,7 @@ spec = do
     describe "quicksort" $ do
         it "sorts integer list" $ do
             quicksort [4, 1, 5, 2, 1, 7, 9, 0] `shouldBe` [0, 1, 1, 2, 4, 5, 7, 9]
+
+    describe "quicksort'" $ do
+        it "sorts integer list" $ do
+            quicksort' [4, 1, 5, 2, 1, 7, 9, 0] `shouldBe` [0, 1, 1, 2, 4, 5, 7, 9]
